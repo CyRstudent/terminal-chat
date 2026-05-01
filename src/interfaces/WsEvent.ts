b66234157c8454a05e34b610a9500d126cc7ea21
CyRstudent <hsmad2022@gmail.com>
2026-05-01 23:33:09 +0200

    feat: WsEvent interface

---
+++ src/interfaces/WsEvent.ts
@@ -0,0 +1,7 @@
+import type { WsEvents } from "../types/WsEvents.js";
+import type { Client } from "../client/Client.js";
+export interface WsEvent<T extends unknown[] = unknown[]> {
+	event: WsEvents;
+	once: boolean;
+	execute: (...args: [T, Client]) => void;
+}
