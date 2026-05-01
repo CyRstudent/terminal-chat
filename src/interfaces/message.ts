489a2ee681e197eb42468e5c0afeb9c2c7d81290
CyRstudent <hsmad2022@gmail.com>
2026-05-01 23:32:17 +0200

    fix: Use semicolons

--- src/interfaces/message.ts
+++ src/interfaces/message.ts
@@ -1,7 +1,7 @@
 import type { Client } from "../client/Client.js";
 
 export interface Message {
-	sender: Client,
-	content: string,
-	date: Date
+	sender: Client;
+	content: string;
+	date: Date;
 }
