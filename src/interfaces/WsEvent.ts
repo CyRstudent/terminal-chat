f7ac1d682eb0153b5601649c763e165cbec96777
CyRstudent <hsmad2022@gmail.com>
2026-05-07 10:19:09 +0200

    fix: Pass type T as a list as well

--- src/interfaces/WsEvent.ts
+++ src/interfaces/WsEvent.ts
@@ -3,5 +3,5 @@ import type { Client } from '../client/Client.js';
 export interface WsEvent<T extends unknown[] = unknown[]> {
 	event: WsEvents;
 	once: boolean;
-	execute: (...args: [T, Client]) => void;
+	execute: (...args: [...T, Client]) => void;
 }
